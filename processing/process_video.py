ddbc9bb7f3855466f152d1318c46ee8485034f55
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-06-02 22:41:03 +0530

    Remove watermarking from video processing
    Removed watermarking functionality and related code.

--- processing/process_video.py
+++ processing/process_video.py
@@ -11,13 +11,11 @@ sys.path.append(str(ROOT))
 from drive.drive_utils import (
     download_file,
     upload_file,
-    delete_file,
     get_drive_link
 )
 
 from processing.extract_keyframes import extract_keyframes
 from processing.analyze_video import analyze_video_frames
-from processing.watermark_video import add_watermark
 from utils.video_status import *
 
 from utils.json_utils import (
@@ -25,12 +23,7 @@ from utils.json_utils import (
     save_videos
 )
 
-from config.settings import (
-    PROCESSED_FOLDER_ID
-)
 
-
-WATERMARK_PATH = ROOT / "assets" / "watermark.png"
 
 
 def process_video(video_record):
@@ -42,11 +35,6 @@ def process_video(video_record):
         original_video = os.path.join(
             temp_dir,
             "source.mp4"
-        )
-
-        watermarked_video = os.path.join(
-            temp_dir,
-            "processed.mp4"
         )
 
         print("Downloading source video...")
@@ -73,45 +61,13 @@ def process_video(video_record):
             frames
         )
 
-        print("Adding watermark...")
-
-        add_watermark(
-            original_video,
-            WATERMARK_PATH,
-            watermarked_video
-        )
-
-        print("Uploading processed video...")
-
-        processed_file_id = upload_file(
-            watermarked_video,
-            PROCESSED_FOLDER_ID
-        )
-
-        processed_link = get_drive_link(
-            processed_file_id
-        )
-
-        print("Deleting source video...")
-
-        delete_file(
-            source_file_id
-        )
-
         mark_processing_completed(
             video_id=video_record["id"],
-            processed_file_id=processed_file_id,
-            processed_link=processed_link,
             analysis=analysis
         )
         
-        mark_source_deleted(
-            video_record["id"]
-        )
 
         return {
-            "processed_file_id": processed_file_id,
-            "processed_link": processed_link,
             "analysis": analysis
         }
 
